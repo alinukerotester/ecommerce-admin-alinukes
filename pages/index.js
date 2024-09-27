a71b96ac64091f490c673b998fb0d9c1b33264ff
alinukerotester <109532807+alinukerotester@users.noreply.github.com>
2024-09-27 22:15:20 +0300

    gata pentru azi

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,20 @@ export default function Home() {
 
   return (
     <Layout>
-      <div className="text-blue-900">
-        Hey, {session?.user?.name}
-        <Image src={session?.user?.image} alt="My Image" />
+      <div className="text-blue-900 flex justify-between">
+        <h2>
+          Hey, <b>{session?.user?.name}</b>
+        </h2>
+        <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
+          <Image
+            src={session?.user?.image}
+            alt="My Image"
+            width={96}
+            height={96}
+            className="w-30 h-30"
+          />
+          <span className="py-1 px-2">{session?.user?.name}</span>
+        </div>
       </div>
     </Layout>
   );
