b041cf06d04f83dc5a906aeb8b29c9dec19f03df
alinukerotester <109532807+alinukerotester@users.noreply.github.com>
2024-11-13 18:21:12 +0200

    domeniu pentru poza activ

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
 import Layout from "@/components/Layout";
 import { useSession } from "next-auth/react";
+import Image from "next/image";
 
 export default function Home() {
   const { data: session } = useSession();
   return (
     <Layout>
-      <div className="text-blue-900">Salut, {session?.user?.name}</div>
+      <div className="text-blue-900 flex">
+        Salut, {session?.user?.name}
+        <Image
+          src={session?.user?.image}
+          alt="poza personala"
+          width={96}
+          height={96}
+        />
+      </div>
     </Layout>
   );
 }
