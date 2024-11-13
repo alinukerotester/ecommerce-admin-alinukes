725922b085b40f692a130e8c62a9d28e88c546b2
alinukerotester <109532807+alinukerotester@users.noreply.github.com>
2024-11-13 14:12:26 +0200

    mongo database

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+
 export default function Home() {
-  return (
-    <div className="bg-blue-900 w-screen h-screen flex items-center">
-      <div className="text-center w-full">
-        <button className="bg-white p-2 px-4 rounded-lg">
-          Login with google
-        </button>
+  const { data: session } = useSession();
+  if (!session) {
+    return (
+      <div className="bg-blue-900 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button
+            onClick={() => signIn("google")}
+            className="bg-white p-2 px-4 rounded-lg"
+          >
+            Login with google
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  return <div>logat pentru distractie {session.user.email} </div>;
 }
