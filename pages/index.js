d6b289c370b194be1bf2284df011b0c36f6af50a
alinukerotester <109532807+alinukerotester@users.noreply.github.com>
2024-11-13 17:35:32 +0200

    functioneaza perfect modificarea

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,5 @@
-import Nav from "@/components/Nav";
-import { useSession, signIn, signOut } from "next-auth/react";
+import Layout from "@/components/Layout";
 
 export default function Home() {
-  const { data: session } = useSession();
-  if (!session) {
-    return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          <button
-            onClick={() => signIn("google")}
-            className="bg-white p-2 px-4 rounded-lg"
-          >
-            Login with google
-          </button>
-        </div>
-      </div>
-    );
-  }
-
-  return (
-    <div className="bg-blue-900 min-h-screen flex">
-      <Nav />
-      <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">
-        logat pentru distractie {session.user.email}{" "}
-      </div>
-    </div>
-  );
+  return <Layout>test</Layout>;
 }
