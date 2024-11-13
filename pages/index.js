b1912786c222cda95e43da5e8fa1714e7451c69f
alinukerotester <109532807+alinukerotester@users.noreply.github.com>
2024-11-13 18:34:57 +0200

    cum se configureaza o pagina de inceput

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,19 @@ export default function Home() {
   const { data: session } = useSession();
   return (
     <Layout>
-      <div className="text-blue-900 flex">
-        Salut, {session?.user?.name}
-        <Image
-          src={session?.user?.image}
-          alt="poza personala"
-          width={96}
-          height={96}
-        />
+      <div className="text-blue-900 flex justify-between">
+        <h2>
+          Salut, <b>{session?.user?.name}</b>
+        </h2>
+        <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
+          <Image
+            src={session?.user?.image}
+            alt="poza personala"
+            width={96}
+            height={96}
+          />
+          <span className="px-2">{session?.user?.name}</span>
+        </div>
       </div>
     </Layout>
   );
